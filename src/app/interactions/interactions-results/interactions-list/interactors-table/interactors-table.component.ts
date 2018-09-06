926c364021c9fe18d2b2bfb0e6840c33e481aa46
elisabetb <barrera@ebi.ac.uk>
2018-09-06 16:44:41 +0100

    NIV-145: Integration of the interactions viewer

--- src/app/interactions/interactions-results/interactions-list/interactors-table/interactors-table.component.ts
+++ src/app/interactions/interactions-results/interactions-list/interactors-table/interactors-table.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { InteractorsSearchResult } from '../../../shared/model/interactions-results/interactor/interactors-search.model';
 
 @Component({
--- src/app/interactions/interactions-results/interactions-viewer/interactions-viewer.component.ts
+++ src/app/interactions/interactions-results/interactions-viewer/interactions-viewer.component.ts
@@ -1,17 +1,53 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, ViewEncapsulation} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+declare const require: any;
+declare const $: any;
+const xiNET = require('expose-loader?xiNET!complexviewer');
+let xlv: any;
+
 
 @Component({
   selector: 'iv-interactions-viewer',
   templateUrl: './interactions-viewer.component.html',
-  styleUrls: ['./interactions-viewer.component.css']
+  styleUrls: ['./interactions-viewer.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class InteractionsViewerComponent implements OnInit {
+export class InteractionsViewerComponent implements AfterViewInit {
 
   @Input() interactorsSelected: string[];
 
-  constructor() { }
+  private _interactionsJSON: any;
 
-  ngOnInit() {
+  constructor(private httpService: HttpClient) { }
+
+  ngAfterViewInit() {
+    this.httpService.get('../../../assets/demo_interactions.json').subscribe(
+      data => {
+        this.interactionsJSON = data;
+
+        if (this.interactionsJSON !== undefined) {
+          console.log('InteractionsJson dummy is ' + this.interactionsJSON);
+
+          $('iv-interactions-viewer').foundation();
+          xlv = new xiNET('graphViewerContainer');
+          xlv.readMIJSON(this.interactionsJSON, true);
+          xlv.autoLayout();
+        }
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+
   }
 
+
+  get interactionsJSON(): any {
+    return this._interactionsJSON;
+  }
+
+  set interactionsJSON(value: any) {
+    this._interactionsJSON = value;
+  }
 }

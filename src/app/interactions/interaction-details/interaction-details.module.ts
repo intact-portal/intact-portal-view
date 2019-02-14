0d91e33dfc0f03c26a93ec89d6a1b8a80f4446e7
elisabetb <barrera@ebi.ac.uk>
2019-02-14 11:24:44 +0000

    NIV-224: move details-info to interaction-details

--- src/app/interactions/interaction-details/interaction-details.module.ts
+++ src/app/interactions/interaction-details/interaction-details.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DetailsDashboardComponent} from './details-dashboard.component';
 import {DetailsTabsComponent} from './details-tabs/details-tabs.component';
-import {InteractionDetailsComponent} from './details-tabs/interaction-details/interaction-details.component';
-import {ExternalXrefsComponent} from './details-tabs/interaction-details/external-xrefs/external-xrefs.component';
-import {CurationAnnotationsComponent} from './details-tabs/interaction-details/curation-annotations/curation-annotations.component';
+import {InteractionDetailsComponent} from './details-tabs/details/interaction-details.component';
+import {ExternalXrefsComponent} from './details-tabs/details/external-xrefs/external-xrefs.component';
+import {CurationAnnotationsComponent} from './details-tabs/details/curation-annotations/curation-annotations.component';
 import {DetailsViewerComponent} from './details-viewer/details-viewer.component';
-import {ExperimentalConditionsComponent} from './details-tabs/interaction-details/experimental-conditions/experimental-conditions.component';
-import {ParametersComponent} from './details-tabs/interaction-details/parameters/parameters.component';
-import {ConfidencesComponent} from './details-tabs/interaction-details/confidences/confidences.component';
+import {ExperimentalConditionsComponent} from './details-tabs/details/experimental-conditions/experimental-conditions.component';
+import {ParametersComponent} from './details-tabs/details/parameters/parameters.component';
+import {ConfidencesComponent} from './details-tabs/details/confidences/confidences.component';
 import {InteractionDetailsRoutingModule} from './interaction-details-routing.module';
-import { ExperimentComponent } from './details-tabs/interaction-details/experiment/experiment.component';
+import { ExperimentComponent } from './details-tabs/details/experiment/experiment.component';
 
 @NgModule({
   imports: [

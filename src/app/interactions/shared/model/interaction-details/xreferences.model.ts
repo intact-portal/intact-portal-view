69a43090e4df89e46e4629b8c149509402d8de77
elisabetb <barrera@ebi.ac.uk>
2019-05-01 11:14:30 +0100

    Few changes to model

--- src/app/interactions/shared/model/interaction-details/xreferences.model.ts
+++ src/app/interactions/shared/model/interaction-details/xreferences.model.ts
@@ -3,10 +3,17 @@ import {CvTerm} from './cv-term.model';
 export class Xreferences {
   private _database: CvTerm;
   private _identifier: string;
+  private _qualifier: CvTerm;
 
-  constructor(database: CvTerm, identifier: string) {
+  // constructor(database: CvTerm, identifier: string) {
+  //   this._database = database;
+  //   this._identifier = identifier;
+  // }
+
+  constructor(database: CvTerm, identifier: string, qualifier: CvTerm) {
     this._database = database;
     this._identifier = identifier;
+    this._qualifier = qualifier;
   }
 
   get database(): CvTerm {
@@ -23,5 +30,13 @@ export class Xreferences {
 
   set identifier(value: string) {
     this._identifier = value;
+  }
+
+  get qualifier(): CvTerm {
+    return this._qualifier;
+  }
+
+  set qualifier(value: CvTerm) {
+    this._qualifier = value;
   }
 }

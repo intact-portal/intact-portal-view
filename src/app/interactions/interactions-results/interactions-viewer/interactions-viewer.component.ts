af72ef9178f1f32cda9e51497fd5d1db6e96571e
ntoro <ntoro@ebi.ac.uk>
2019-06-07 16:39:36 +0100

    Fitst attempt to integrate interactionNetwork component.

--- src/app/interactions/interactions-results/interactions-viewer/interactions-viewer.component.ts
+++ src/app/interactions/interactions-results/interactions-viewer/interactions-viewer.component.ts
@@ -1,10 +1,11 @@
 import {AfterViewInit, Component, Input, ViewEncapsulation} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {NetworkSearchService} from '../../shared/service/network-search.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 declare const require: any;
 declare const $: any;
-const xiNET = require('expose-loader?xiNET!complexviewer');
-let xlv: any;
+const IntactGraph = require('expose-loader?IntactGraph!intact_network');
+let ig: any;
 
 
 @Component({
@@ -15,14 +16,44 @@ let xlv: any;
 })
 export class InteractionsViewerComponent implements AfterViewInit {
 
+  private _term: string;
+  private _speciesNameFilter: string[];
+  private _interactorTypeFilter: string[];
+  private _interactionTypeFilter: string[];
+  private _detectionMethodFilter: string[];
+  private _hostOrganismFilter: string[];
+  private _negativeFilter: boolean;
+  private _miScoreMin: any;
+  private _miScoreMax: any;
+
   @Input() interactorsSelected: string[];
   @Input() interactionSelected: string;
 
   private _interactionsJSON: any;
 
-  constructor(private httpService: HttpClient) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private networkSearchService: NetworkSearchService) { }
 
   ngAfterViewInit() {
+    this.route.queryParams
+      .filter(params => params.query)
+      .subscribe(params => {
+        this.term = params.query;
+        this.interactorTypeFilter = params.interactorType ? params.interactorType.split('+') : [];
+        this.speciesNameFilter = params.species ? params.species.split('+') : [];
+        this.hostOrganismFilter = params.hostOrganism ? params.hostOrganism.split('+') : [];
+        this.interactionTypeFilter = params.interactionType ? params.interactionType.split('+') : [];
+        this.detectionMethodFilter = params.detectionMethod ? params.detectionMethod.split('+') : [];
+        this.negativeFilter = params.negativeInteraction ? params.negativeInteraction : false;
+        this.miScoreMax = params.miScoreMax ? params.miScoreMax : 1;
+        this.miScoreMin = params.miScoreMin ? params.miScoreMin : 0;
+        this.interactorsSelected = params.interactorsSelected ? params.interactorsSelected.split('+') : [];
+        // this.interactionSelected = params.interactionSelected ? params.interactionSelected : '';
+
+        this.requestIntactNetworkDetails();
+      });
+
     // this.httpService.get('../../../assets/demo_interactions.json').subscribe(
     //   data => {
     //     this.interactionsJSON = data;
@@ -40,9 +71,97 @@ export class InteractionsViewerComponent implements AfterViewInit {
     //     console.log (err.message);
     //   }
     // );
-
   }
 
+  private requestIntactNetworkDetails() {
+    this.networkSearchService.getInteractionNetwork(
+      this.term,
+      this.hostOrganismFilter,
+      this.interactionTypeFilter,
+      this.detectionMethodFilter,
+      this.miScoreMin,
+      this.miScoreMax,
+      this.negativeFilter
+    ).subscribe(data => {
+      this.interactionsJSON = data;
+      console.log('Data loaded');
+      ig = new IntactGraph();
+      ig.loadJsonAndProcess(this.interactionsJSON, true);
+
+    })
+  }
+
+  get term(): string {
+    return this._term;
+  }
+
+  set term(value: string) {
+    this._term = value;
+  }
+
+  get speciesNameFilter(): string[] {
+    return this._speciesNameFilter;
+  }
+
+  set speciesNameFilter(value: string[]) {
+    this._speciesNameFilter = value;
+  }
+
+  get interactorTypeFilter(): string[] {
+    return this._interactorTypeFilter;
+  }
+
+  set interactorTypeFilter(value: string[]) {
+    this._interactorTypeFilter = value;
+  }
+
+  get interactionTypeFilter(): string[] {
+    return this._interactionTypeFilter;
+  }
+
+  set interactionTypeFilter(value: string[]) {
+    this._interactionTypeFilter = value;
+  }
+
+  get detectionMethodFilter(): string[] {
+    return this._detectionMethodFilter;
+  }
+
+  set detectionMethodFilter(value: string[]) {
+    this._detectionMethodFilter = value;
+  }
+
+  get hostOrganismFilter(): string[] {
+    return this._hostOrganismFilter;
+  }
+
+  set hostOrganismFilter(value: string[]) {
+    this._hostOrganismFilter = value;
+  }
+
+  get negativeFilter(): boolean {
+    return this._negativeFilter;
+  }
+
+  set negativeFilter(value: boolean) {
+    this._negativeFilter = value;
+  }
+
+  get miScoreMin(): any {
+    return this._miScoreMin;
+  }
+
+  set miScoreMin(value: any) {
+    this._miScoreMin = value;
+  }
+
+  get miScoreMax(): any {
+    return this._miScoreMax;
+  }
+
+  set miScoreMax(value: any) {
+    this._miScoreMax = value;
+  }
 
   get interactionsJSON(): any {
     return this._interactionsJSON;

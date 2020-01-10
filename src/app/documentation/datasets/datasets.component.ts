648c384d2e80d6a178f28b74b5212025798e645e
ntoro <ntoro@ebi.ac.uk>
2020-01-10 14:10:28 +0000

    New datasets component for the documentation.

---
+++ src/app/documentation/datasets/datasets.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Title} from '@angular/platform-browser';
+
+declare const $: any;
+
+@Component({
+  selector: 'iv-datasets',
+  templateUrl: './datasets.component.html',
+  styleUrls: ['./datasets.component.css']
+})
+export class DatasetsComponent implements OnInit {
+
+  curated = environment.intact_portal_documentation_url + 'dataset-descriptions/curated-datasets.md';
+  computationally = environment.intact_portal_documentation_url + 'dataset-descriptions/computational-datasets.md';
+  species = environment.intact_portal_documentation_url + 'dataset-descriptions/species-based-datasets.md';
+  mutations = environment.intact_portal_documentation_url + 'dataset-descriptions/mutations.md';
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('IntAct - Datasets');
+    $('iv-datasets').foundation();
+  }
+
+}

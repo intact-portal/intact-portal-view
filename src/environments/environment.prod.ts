01e1c9af6581b5514c93a7eb8a887ba6e8cfe0aa
ntoro <ntoro@ebi.ac.uk>
2020-01-10 16:01:17 +0000

    Removes pmid from the intact ftp urls to make them more reusable.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 // Environment for production
 const EBI_BASE_URL = 'https://www.ebi.ac.uk/';
 const LICENSE_URL = 'https://creativecommons.org/licenses/by/4.0/';
@@ -11,8 +15,8 @@ export const environment = {
   // analytics_id: 'UA-XXXXXX-XX',
   ebi_base_url: EBI_BASE_URL,
   license_url: LICENSE_URL,
-  intact_psi25_url : INTACT_FTP_BASE_URL + '/current/psi25/pmid',
-  intact_psi30_url : INTACT_FTP_BASE_URL + '/current/psi30/pmid',
+  intact_psi25_url : INTACT_FTP_BASE_URL + '/current/psi25',
+  intact_psi30_url : INTACT_FTP_BASE_URL + '/current/psi30',
   intact_psimitab_url : INTACT_FTP_BASE_URL + '/current/psimitab/pmid',
   intact_dotm_url: EBI_BASE_URL + '/~intact/site/dotm/dotm-1.1.xml',
   intact_portal_url: EBI_BASE_URL + '/intact',

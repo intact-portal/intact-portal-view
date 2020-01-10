7bd0b1c6d453dfa400cbca793ab292634868ed84
ntoro <ntoro@ebi.ac.uk>
2020-01-10 13:51:15 +0000

    Moves about component under documentation component. Allows bookmarking in the tabs with the help of data-deep-link in foundation. Performs some cleaning.

--- src/app/documentation/about/about.component.ts
+++ src/app/documentation/about/about.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
-import {environment} from '../../environments/environment';
+import {environment} from '../../../environments/environment';
 
 declare const $: any;
 
@@ -9,24 +9,23 @@ declare const $: any;
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit, AfterViewInit {
+export class AboutComponent implements OnInit {
 
   overview = environment.intact_portal_documentation_url + 'about/overview.md';
   collaborators = environment.intact_portal_documentation_url + 'about/collaborators.md';
   funding = environment.intact_portal_documentation_url + 'about/funding.md';
+  statistics = `# Statistics`; // Temporary until we have the statistics page
   publications = environment.intact_portal_documentation_url + 'about/publications.md';
   license_privacy = environment.intact_portal_documentation_url + 'about/license-privacy.md';
   conceptual_design = environment.intact_portal_documentation_url + 'about/conceptual-design.md';
   sab = environment.intact_portal_documentation_url + 'about/scientific-advisory-board.md';
   cite_us = environment.intact_portal_documentation_url + 'about/cite-us.md';
 
-  constructor(private titleService: Title) { }
-
-  ngOnInit() {
-    this.titleService.setTitle('About');
+  constructor(private titleService: Title) {
   }
 
-  ngAfterViewInit(): void {
+  ngOnInit() {
+    this.titleService.setTitle('IntAct - About');
     $('iv-about').foundation();
   }
 

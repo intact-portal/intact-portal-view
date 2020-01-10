65c0244aaf8ff89c0f231f2042eeaabe7552f396
ntoro <ntoro@ebi.ac.uk>
2020-01-10 14:43:57 +0000

    New technical-corner component for the documentation.

---
+++ src/app/documentation/technical-corner/technical-corner.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Title} from '@angular/platform-browser';
+
+declare const $: any;
+
+@Component({
+  selector: 'iv-technical-corner',
+  templateUrl: './technical-corner.component.html',
+  styleUrls: ['./technical-corner.component.css']
+})
+export class TechnicalCornerComponent implements OnInit {
+
+  apis = environment.intact_portal_documentation_url + 'technical-corner/apis.md';
+  source_code = environment.intact_portal_documentation_url + 'technical-corner/source-code.md';
+  tools = environment.intact_portal_documentation_url + 'technical-corner/tools.md';
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('IntAct - Technical Corner');
+    $('iv-technical-corner').foundation();
+  }
+
+}

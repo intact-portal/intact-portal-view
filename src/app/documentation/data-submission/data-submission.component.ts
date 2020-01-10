2832f08e2db46c8816a2963769212af3682b230b
ntoro <ntoro@ebi.ac.uk>
2020-01-10 14:06:07 +0000

    New data-submission component for the documentation.

---
+++ src/app/documentation/data-submission/data-submission.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Title} from '@angular/platform-browser';
+
+declare const $: any;
+
+@Component({
+  selector: 'iv-data-submission',
+  templateUrl: './data-submission.component.html',
+  styleUrls: ['./data-submission.component.css']
+})
+export class DataSubmissionComponent implements OnInit {
+
+  how_to_submit = environment.intact_portal_documentation_url + 'data-submission/how-to-submit.md';
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('IntAct - Data Submission');
+    $('iv-datasets').foundation();
+  }
+}

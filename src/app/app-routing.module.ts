8492aa872f1f30352ce49d6e89267c29cf30de96
ntoro <ntoro@ebi.ac.uk>
2020-01-10 16:10:10 +0000

    Removes components than have been moved to other submodules after the documentation refactoring. Adds new Interactomes component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { DownloadComponent } from './download/download.component';
-import { FaqComponent } from './faq/faq.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DownloadComponent} from './download/download.component';
+import {InteractomesComponent} from './interactomes/interactomes.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: 'app/home-dashboard/home-dashboard.module#HomeDashboardModule'},
   {path: 'search', loadChildren: 'app/interactions/interactions-results/interactions-results.module#InteractionsResultsModule'},
   {path: 'details', loadChildren: 'app/interactions/interaction-details/interaction-details.module#InteractionDetailsModule'},
-  {path: 'about', component: AboutComponent},
   {path: 'download', component: DownloadComponent},
-  {path: 'faq', component: FaqComponent},
-]
+  {path: 'interactomes', component: InteractomesComponent},
+];
 
 @NgModule({
   exports: [ RouterModule ],
   imports: [ RouterModule.forRoot(routes, {useHash: false}) ],
 })
+
 export class AppRoutingModule { }

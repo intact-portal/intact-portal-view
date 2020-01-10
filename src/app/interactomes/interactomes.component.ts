e4a63cba75889bf9aab6ed1487dcce6a61b994e0
ntoro <ntoro@ebi.ac.uk>
2020-01-10 13:45:50 +0000

    New interactome component for listing the 16 species with the higher number of interactions.

---
+++ src/app/interactomes/interactomes.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {Interactome} from './interactome.model';
+import {Title} from '@angular/platform-browser';
+import {InteractomesConstants} from './interactomes-constants';
+
+@Component({
+  selector: 'iv-interactomes',
+  templateUrl: './interactomes.component.html',
+  styleUrls: ['./interactomes.component.css']
+})
+export class InteractomesComponent implements OnInit {
+
+  private _interactomes: Interactome[] = [];
+
+  constructor(private titleService: Title) {
+    this.interactomes = InteractomesConstants.interactomes;
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle('IntAct - Interactomes');
+  }
+
+  get interactomes(): Interactome[] {
+    return this._interactomes;
+  }
+
+  set interactomes(value: Interactome[]) {
+    this._interactomes = value;
+  }
+}

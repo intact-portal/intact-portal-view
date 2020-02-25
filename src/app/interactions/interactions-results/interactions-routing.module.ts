135538373b74730f11881686929c80926734b314
ntoro <ntoro@ebi.ac.uk>
2020-02-25 16:49:28 +0000

    Housekeeping code tasks. Remove unused files. Reformat some file to follow the IntelliJ automatic formatting.

--- src/app/interactions/interactions-results/interactions-routing.module.ts
+++ src/app/interactions/interactions-results/interactions-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {InteractionsResultsComponent} from './interactions-results.component';
 
 const routes: Routes = [

f0adffea127f981e288c81f273a5d8e281fd1a87
EliotRagueneau <orkeliot@gmail.com>
2020-10-21 19:19:21 +0100

    Pagination of autosuggestion WIP

---
+++ src/app/home-dashboard/shared/pagination.model.ts
@@ -0,0 +1,28 @@
+export class Pagination<T = any> {
+  content: T;
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  sort: {
+    empty: boolean,
+    sorted: boolean,
+    unsorted: boolean
+  }
+  pageable: {
+    offset: number,
+    pageNumber: number,
+    pageSize: number,
+    paged: boolean,
+    unpaged: boolean,
+    sort: {
+      empty: boolean,
+      sorted: boolean,
+      unsorted: boolean
+    }
+  }
+}

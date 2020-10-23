0ef010622cdf86b7c02707daf3dfee7cf62170a1
EliotRagueneau <orkeliot@gmail.com>
2020-10-23 16:20:12 +0100

    NIV-489: id => ID & Ac => AC

--- src/app/interactions/shared/model/tables/feature-table.model.ts
+++ src/app/interactions/shared/model/tables/feature-table.model.ts
@@ -2,16 +2,15 @@ import {Table} from "./table.model";
 import {Column} from "./column.model";
 
 export class FeatureTable extends Table {
-  public readonly select = new Column(this, 'Select', 'participantName');
-  public readonly ac = new Column(this, 'Ac', 'featureAc');
+  public readonly ac = new Column(this, 'AC', 'featureAc');
   public readonly name = new Column(this, 'Name', 'name');
-  public readonly type = new Column(this, 'Type', 'type.shortName');
+  public readonly type = new Column(this, 'Type', 'type');
   public readonly role = new Column(this, 'Role', 'role');
   public readonly rangePositions = new Column(this, 'Range Positions', 'ranges');
   public readonly linkedFeatures = new Column(this, 'Linked Features', 'linkedFeatures');
   public readonly participantName = new Column(this, 'Participant Name', 'participantName');
-  public readonly participantIdentifier = new Column(this, 'Participant Identifier', 'participant.identifier');
-  public readonly participantAc = new Column(this, 'Participant Ac', 'participantAc');
+  public readonly participantIdentifier = new Column(this, 'Participant Identifier', 'participant');
+  public readonly participantAc = new Column(this, 'Participant AC', 'participantAc');
   public readonly detectionMethods = new Column(this, 'Detection Methods', 'detectionMethods');
   public readonly parameters = new Column(this, 'Parameters', 'parameters');
   public readonly identifiers = new Column(this, 'Identifiers', 'identifiers');
--- src/app/interactions/shared/model/tables/participant-table.model.ts
+++ src/app/interactions/shared/model/tables/participant-table.model.ts
@@ -2,20 +2,21 @@ import {Table} from "./table.model";
 import {Column} from "./column.model";
 
 export class ParticipantTable extends Table {
-  public readonly select = new Column(this, 'Select', 'participantName');
-  public readonly ac = new Column(this, 'Ac', 'participantAc');
-  public readonly type = new Column(this, 'Type', 'type.shortName');
-  public readonly identifier = new Column(this, 'Identifier', 'participantId.identifier');
+  public readonly expand = new Column(this, 'Expand', 'participantName');
+  public readonly name = new Column(this, 'Name', 'participantName');
+  public readonly ac = new Column(this, 'AC', 'participantAc');
+  public readonly type = new Column(this, 'Type', 'type');
+  public readonly identifier = new Column(this, 'Identifier', 'participantId');
   public readonly aliases = new Column(this, 'Aliases', 'aliases');
   public readonly description = new Column(this, 'Description', 'description');
-  public readonly species = new Column(this, 'Species', 'species.scientificName');
-  public readonly expressionSystem = new Column(this, 'Expression System', 'expressionSystem.scientificName');
+  public readonly species = new Column(this, 'Species', 'species');
+  public readonly expressionSystem = new Column(this, 'Expression System', 'expressionSystem');
   public readonly detectionMethods = new Column(this, 'Detection Methods', 'detectionMethod');
-  public readonly experimentalRole = new Column(this, 'Experimental Role', 'experimentalRole.shortName');
-  public readonly biologicalRole = new Column(this, 'Biological Role', 'biologicalRole.shortName');
+  public readonly experimentalRole = new Column(this, 'Experimental Role', 'experimentalRole');
+  public readonly biologicalRole = new Column(this, 'Biological Role', 'biologicalRole');
   public readonly experimentalPreparations = new Column(this, 'Experimental Preparations', 'experimentalPreparations');
   public readonly parameters = new Column(this, 'Parameters', 'parameters');
   public readonly confidences = new Column(this, 'Confidences', 'confidences');
   public readonly crossReferences = new Column(this, 'Cross References', 'xrefs');
-  public readonly annotations = new Column(this, 'annotations', 'annotations');
+  public readonly annotations = new Column(this, 'Annotations', 'annotations');
 }

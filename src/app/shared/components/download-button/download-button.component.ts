392b15cfc699e5991803ee463b26110668fd032b
EliotRagueneau <orkeliot@gmail.com>
2021-01-19 18:27:09 +0000

    download button top to bottom

--- src/app/shared/components/download-button/download-button.component.ts
+++ src/app/shared/components/download-button/download-button.component.ts
@@ -1,32 +1,55 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'ip-download-button',
   templateUrl: './download-button.component.html',
   styleUrls: ['./download-button.component.css']
 })
-export class DownloadButtonComponent implements OnInit {
+export class DownloadButtonComponent implements OnInit, AfterViewInit {
 
   @Input()
-  formats: {name: string, url: string}[] = [];
+  formats: { name: string, url: string }[] = [];
 
-  constructor() { }
+  @ViewChild('container')
+  private container: ElementRef;
+  private $container: JQuery;
+  @ViewChild('icon')
+  private icon: ElementRef;
+  private $icon: JQuery;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  showDownloads() {
-    $('.download-buttons-container').stop().animate({width:'show'},350)
-    $('.download-buttons .icon-download').stop().animate({
-      borderTopLeftRadius: 0,
-      borderBottomLeftRadius: 0},350)
+  ngAfterViewInit(): void {
+    this.$container = $(this.container.nativeElement);
+    this.$icon = $(this.icon.nativeElement);
+    this.$icon.width(Math.max(this.$container.outerWidth(false), this.$icon.width()));
   }
 
-  hideDownloads() {
-    $('.download-buttons-container').stop().animate({width:'hide'},350)
-    $('.download-buttons .icon-download').stop().animate({
-      borderTopLeftRadius: '0.2rem',
-      borderBottomLeftRadius: '0.2rem'},350)
 
+  showDownloads() {
+    this.$icon.stop().animate({
+      borderBottomRightRadius: 0,
+      borderBottomLeftRadius: 0
+    }, {
+      duration: 50,
+      complete: () => this.$container.stop().slideDown(350)
+    });
+  }
+
+
+  hideDownloads() {
+    this.$container.stop().slideUp({
+      duration: 350,
+      complete: () => {
+        this.$icon.stop().animate({
+          borderBottomRightRadius: '0.3rem',
+          borderBottomLeftRadius: '0.3rem'
+        }, 50)
+      }
+    });
   }
 }

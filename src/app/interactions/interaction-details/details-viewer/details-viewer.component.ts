02878a9b9d675683bb4199ea21c049e568341fcf
EliotRagueneau <orkeliot@gmail.com>
2021-01-28 19:24:58 +0000

    Details page rework based on latest complexviewer

--- src/app/interactions/interaction-details/details-viewer/details-viewer.component.ts
+++ src/app/interactions/interaction-details/details-viewer/details-viewer.component.ts
@@ -8,8 +8,8 @@ import * as complexviewer from 'complexviewer';
 import {InteractionParticipantsService} from "../shared/service/interaction-participants.service";
 import {Participant} from "../shared/model/participant.model";
 import {Subscription} from "rxjs";
+import {NodeShape} from "../../shared/model/network-shapes/node-shape";
 
-declare const require: any;
 declare const $: any;
 export var viewer: any;
 
@@ -27,18 +27,19 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
   @Output() error: EventEmitter<HttpErrorResponse> = new EventEmitter<HttpErrorResponse>();
   private _interactionData: any;
 
-  private SMALL_MOL = require('../../../../assets/images/detailsViewer/svgForKey/smallMol.svg');
-  private GENE = require('../../../../assets/images/detailsViewer/svgForKey/gene.svg');
-  private DNA = require('../../../../assets/images/detailsViewer/svgForKey/DNA.svg');
-  private RNA = require('../../../../assets/images/detailsViewer/svgForKey/RNA.svg');
-  private COMPLEX = require('../../../../assets/images/detailsViewer/svgForKey/RNA.svg');
-  private PROTEIN_BLOB = require('../../../../assets/images/detailsViewer/svgForKey/proteinBlob.svg');
-  private PROTEIN_BAR = require('../../../../assets/images/detailsViewer/svgForKey/proteinBar.svg');
-  private _annotationSelected: string = 'MI Features';
+  annotations = {
+    "MI Features": true,
+    "UniprotKB": false,
+    "Superfamily": false,
+    "Interactor": false,
+  };
 
   private _colorLegendGroups: { name: string, legends: ColorLegend[] }[] = [];
   private proteinUpdateSubscription: Subscription;
   private notifyViewerOfUpdates = true;
+  NodeShape = NodeShape;
+
+  private nodeTypes: Set<string> = new Set<string>();
 
   constructor(private interactionsDetailsService: InteractionsDetailsService, private participantsService: InteractionParticipantsService) {
   }
@@ -47,10 +48,15 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     this.requestInteractionViewerDetails();
     $('ip-interactions-viewer').foundation();
+    $('.button-group.expanded').foundation();
   }
 
   ngOnDestroy(): void {
     this.proteinUpdateSubscription.unsubscribe();
+  }
+
+  public hasNodeShapeOf(type: string): boolean {
+    return this.nodeTypes ? this.nodeTypes.has(type) : false;
   }
 
   private requestInteractionViewerDetails() {
@@ -64,9 +70,9 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
             viewer.readMIJSON(this.interactionData, true);
             viewer.autoLayout();
             this.expandAll();
-            this.participantsService.initParticipants(viewer.getExpandedParticipants())
-            this.updateColorLegend(viewer.getColorKeyJson())
-
+            this.participantsService.initParticipants(viewer.getExpandedParticipants());
+            this.updateColorLegend(viewer.getColorKeyJson());
+            this.collectTypes();
             viewer.addExpandListener((expandedParticipants: Participant[]) => {
               this.notifyViewerOfUpdates = false;
               this.participantsService.updateProteinsStatus(expandedParticipants)
@@ -96,12 +102,10 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
     this.participantsService.collapseAllProteins();
   }
 
-  changeAnnotations(event): void {
-    viewer.showAnnotations(this.annotationSelected, false);
-    this.annotationSelected = event.target.value;
-    if (this.annotationSelected !== 'None') {
-      this.updateColorLegend(viewer.showAnnotations(this.annotationSelected, true));
-    }
+  onChangeAnnotation(value: string) {
+    let display = !this.annotations[value];
+    this.annotations[value] = display;
+    this.updateColorLegend(viewer.showAnnotations(value, display));
   }
 
   private updateColorLegend(legendData: { [p: string]: ColorLegend[] }) {
@@ -124,6 +128,51 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
     }
   }
 
+  private static types = [
+    {
+      set: new Set(["MI:0326", "MI:0327"]),
+      name: 'protein'
+    },
+    {
+      set: new Set(["MI:1100", "MI:0904", "MI:0328"]),
+      name: 'bioactive entity'
+    },
+    {
+      set: new Set(["MI:0319", "MI:0681", "MI:0680"]),
+      name: 'dna'
+    },
+    {
+      set: new Set(["MI:0320", "MI:0321", "MI:0322", "MI:0323", "MI:2190", "MI:0324", "MI:0679", "MI:0608", "MI:0611", "MI:0610", "MI:0607", "MI:0609", "MI:0325", "IA:2966", "MI:0318"]),
+      name: 'rna'
+    },
+    {
+      set: new Set(['MI:0250']),
+      name: 'gene'
+    },
+    {
+      set: new Set(['MI:1299', 'MI:0233', 'MI:0315', 'MI:0316', 'MI:1298', 'MI:1301', 'MI:1302', 'MI:1300', 'MI:1303']),
+      name: 'complex'
+    }
+  ];
+
+  private collectTypes() {
+    console.log(viewer.interactors)
+    for (let datum of this.interactionData.data) {
+      console.log(datum);
+      if (datum.object !== "interaction") break;
+      for (let jsonParticipant of datum.participants) {
+        const intRef = jsonParticipant.interactorRef;
+        const interactor = viewer.interactors.get(intRef);
+        for (let type of DetailsViewerComponent.types) {
+          if (type.set.has(interactor.type.id)) {
+            this.nodeTypes.add(type.name);
+            break;
+          }
+        }
+      }
+    }
+  }
+
   get interactionData(): any {
     return this._interactionData;
   }
@@ -132,16 +181,13 @@ export class DetailsViewerComponent implements AfterViewInit, OnDestroy {
     this._interactionData = value;
   }
 
-  get annotationSelected(): string {
-    return this._annotationSelected;
-  }
-
-  set annotationSelected(value: string) {
-    this._annotationSelected = value;
-  }
 
   get colorLegendGroups(): { name: string; legends: ColorLegend[] }[] {
     return this._colorLegendGroups;
+  }
+
+  resetLayout() {
+    viewer.autoLayout()
   }
 }
 

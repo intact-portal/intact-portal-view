de0ad4de371814e2f0501eea615951966036a35c
EliotRagueneau <orkeliot@gmail.com>
2021-03-26 21:00:10 +0000

    Add download button via forms to let the browser handle the download

---
+++ src/app/interactions/interactions-results/interactions-filters/download-form/download-form.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {environment} from "../../../../../environments/environment";
+import {Format} from "../../../shared/model/download/format.model";
+import {Filter, FilterService} from "../../../shared/service/filter.service";
+import {SearchService} from "../../../../home-dashboard/search/service/search.service";
+import {NetworkSelectionService} from "../../../shared/service/network-selection.service";
+
+const baseURL = environment.intact_portal_ws;
+
+@Component({
+  selector: 'ip-download-form',
+  templateUrl: './download-form.component.html',
+  styleUrls: ['./download-form.component.css']
+})
+export class DownloadFormComponent implements OnInit {
+  @Input() format: Format = Format.json;
+  @Input() last: boolean = false;
+
+  filterTypes = Filter;
+  readonly URL = `${baseURL}/graph/export/interaction/list`;
+
+  constructor(public filters: FilterService, public search: SearchService, public selection: NetworkSelectionService) {
+  }
+
+  ngOnInit() {
+  }
+
+}

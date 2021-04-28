06bc5e57c180dea93fc4a54a2fd91eaaffe1551d
EliotRagueneau <159753.AZErty>
2021-04-28 11:20:22 +0100

    tab to txt

--- src/app/interactions/shared/model/download/format.model.ts
+++ src/app/interactions/shared/model/download/format.model.ts
@@ -1,9 +1,9 @@
 export class Format {
   private static _instances: Format[] = [];
 
-  public static tab2_5 = new Format('tab', 'miTab25', 'miTab<sub>2.5</sub>');
-  public static tab2_6 = new Format('tab', 'miTab26', 'miTab<sub>2.6</sub>');
-  public static tab2_7 = new Format('tab', 'miTab27', 'miTab<sub>2.7</sub>');
+  public static tab2_5 = new Format('txt', 'miTab25', 'miTab<sub>2.5</sub>');
+  public static tab2_6 = new Format('txt', 'miTab26', 'miTab<sub>2.6</sub>');
+  public static tab2_7 = new Format('txt', 'miTab27', 'miTab<sub>2.7</sub>');
   public static xml2_5 = new Format('xml', 'miXML25', 'miXML<sub>2.5</sub>');
   public static xml3_0 = new Format('xml', 'miXML30', 'miXML<sub>3.0</sub>');
   public static json = new Format('json', 'miJSON', 'miJSON<sub>1.0</sub>');

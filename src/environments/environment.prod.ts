263bbf9065bd32a330d4e6de67ba8d86146e361c
EliotRagueneau <159753.AZErty>
2021-04-28 18:00:27 +0100

    Programmatically feed base href to contextPath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,18 +8,14 @@ const EBI_URL = 'https://www.ebi.ac.uk';
 const LICENSE_URL = 'https://creativecommons.org/licenses/by/4.0/';
 const INTACT_FTP_BASE_URL = 'https://ftp.ebi.ac.uk/pub/databases/intact/';
 const INTACT_PORTAL_DOCUMENTATION_URL = 'https://raw.githubusercontent.com/Intact-Portal/intact-portal-documentation/master/';
-// TODO inject the context path programmatically.
-// No trailing bar
-const CONTEXT_PATH = '/intact/dev'
 
 export const environment = {
   production: true,
   evn: 'prod',
-
-  // analytics_id: 'UA-XXXXXX-XX',
+  analytics_id: 'UA-XXXXXX-XX',
   ebi_base_url: EBI_BASE_URL,
   ebi_url: EBI_URL,
-  context_path: CONTEXT_PATH,
+  context_path: '',
   license_url: LICENSE_URL,
   intact_psi25_url : INTACT_FTP_BASE_URL + '/current/psi25',
   intact_psi30_url : INTACT_FTP_BASE_URL + '/current/psi30',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,10 +12,6 @@ const EBI_URL = 'https://www.ebi.ac.uk';
 const LICENSE_URL = 'https://creativecommons.org/licenses/by/4.0/';
 const INTACT_FTP_BASE_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/intact';
 const INTACT_PORTAL_DOCUMENTATION_URL = 'https://raw.githubusercontent.com/Intact-Portal/intact-portal-documentation/master/';
-// TODO inject the context path programmatically.
-// No trailing bar
-const CONTEXT_PATH = '/intact/dev'
-// const CONTEXT_PATH = '/intact-portal-view'
 
 export const environment = {
   production: false,
@@ -23,7 +19,7 @@ export const environment = {
   analytics_id: '2381917528',
   ebi_base_url: EBI_BASE_URL,
   ebi_url: EBI_URL,
-  context_path: CONTEXT_PATH,
+  context_path: '',
   license_url: LICENSE_URL,
   intact_psi25_url : INTACT_FTP_BASE_URL + '/current/psi25',
   intact_psi30_url : INTACT_FTP_BASE_URL + '/current/psi30',

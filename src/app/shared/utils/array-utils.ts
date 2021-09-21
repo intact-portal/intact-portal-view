6966cab74fa50a650b0de5a4c90bf44bb153e317
EliotRagueneau <159753.AZErty>
2021-09-21 15:46:53 +0100

    Linter

--- src/app/shared/utils/array-utils.ts
+++ src/app/shared/utils/array-utils.ts
@@ -1,7 +1,7 @@
-export function groupBy<T, K>(array: T[], groupBy: (element: T) => K): { group: K; elements: T[] }[] {
+export function groupBy<T, K>(array: T[], classifier: (element: T) => K): { group: K; elements: T[] }[] {
   return Array.from(array.reduce((groups: Map<string, { group: K, elements: T[] }>, value) => {
-    let group = groupBy(value);
-    let key = JSON.stringify(group);
+    const group = classifier(value);
+    const key = JSON.stringify(group);
     if (groups.has(key)) {
       groups.get(key).elements.push(value);
     } else {

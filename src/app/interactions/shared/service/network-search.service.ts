bbbad4ba395671d67ae7ab506d005323593dcbad
EliotRagueneau <159753.AZErty>
2021-09-22 15:03:04 +0100

    NIV-717: Improve error messaging on network viewer: Display the error if not 403

--- src/app/interactions/shared/service/network-search.service.ts
+++ src/app/interactions/shared/service/network-search.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs/Rx';
 import {environment} from '../../../../environments/environment';
 import {NetworkLegend} from '../model/interaction-legend/network-legend';
 import {GoogleAnalyticsService} from '../../../shared/service/google-analytics/google-analytics.service';
@@ -25,11 +25,12 @@ export class NetworkSearchService {
 
 
   private handleError(err: HttpErrorResponse | any): Observable<any> {
-    this.reporter.reportError(err);
-    if (err.error instanceof Error) {
-      return Observable.throw(err);
-    } else {
+    if (this.reporter) {
+      this.reporter.reportError(err);
+    }
+    if (!(err.error instanceof Error)) {
       console.error(err.message ? err.message : err.toString());
     }
+    return Observable.throw(err);
   }
 }

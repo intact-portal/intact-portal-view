435d1a1782491b0151a98e841e06e416ac507a6c
EliotRagueneau <159753.AZErty>
2021-09-30 10:56:27 +0100

    Fix SubscriberComponent so that 'this' is correctly set => fix batch search

--- src/app/shared/utils/observer-utils.ts
+++ src/app/shared/utils/observer-utils.ts
@@ -1,7 +1,6 @@
 import {Subscription} from 'rxjs/Subscription';
 import {Observable} from 'rxjs/Observable';
 import {PartialObserver} from 'rxjs/Observer';
-import {toSubscriber} from 'rxjs/util/toSubscriber';
 import {OnDestroy} from '@angular/core';
 
 
@@ -16,6 +15,15 @@ export class SubscriberComponent implements OnDestroy {
                observerOrNext?: PartialObserver<T> | ((value: T) => void),
                error?: (error: any) => void,
                complete?: () => void) {
-    this.subscriptions.push(observable.subscribe(toSubscriber(observerOrNext, error, complete)));
+    if (SubscriberComponent.isObserver(observerOrNext)) {
+      this.subscriptions.push(observable.subscribe(observerOrNext));
+    } else {
+      this.subscriptions.push(observable.subscribe(observerOrNext.bind(this), error ? error.bind(this) : null, complete ? complete.bind(this) : null));
+    }
+  }
+
+  private static isObserver<T>(toBeDetermined: PartialObserver<T> | ((value: T) => void)): toBeDetermined is PartialObserver<T> {
+    const asObserver = toBeDetermined as PartialObserver<T>;
+    return !!(asObserver.next || asObserver.error || asObserver.complete || asObserver.closed);
   }
 }

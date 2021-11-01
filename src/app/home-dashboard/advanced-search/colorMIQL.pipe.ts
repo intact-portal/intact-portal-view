0f41fedea80cc07b35cfb0045577642360768842
EliotRagueneau <159753.AZErty>
2021-11-01 17:17:22 +0000

    :children_crossing: Fix issue with colored advanced search using https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/

--- src/app/home-dashboard/advanced-search/colorMIQL.pipe.ts
+++ src/app/home-dashboard/advanced-search/colorMIQL.pipe.ts
@@ -10,8 +10,8 @@ export class ColorMIQLPipe implements PipeTransform {
 
   public static colorCodes: ColorCode[] = [
     {regex: /(".+")/g, class: 'miql-string'},
-    {regex: RegExp(`(${AdvancedQueryHelper.keywords.map(keyword => keyword.replace('-', '\-')).join('|')})`, 'g'), class: 'miql-field'},
-    {regex: /(AND[ \u00a0]|NOT[ \u00a0]|OR[ \u00a0]|TO[ \u00a0])/ig, class: 'miql-operator'},
+    {regex: RegExp(`(${AdvancedQueryHelper.keywords.map(keyword => keyword.replace('-', '\-') + '(?=:|$)').join('|')})`, 'gm'), class: 'miql-field'},
+    {regex: /(AND(?=\s|<br>)|NOT(?=\s|<br>)|OR(?=\s|<br>)|TO(?=\s|<br>))/img, class: 'miql-operator'},
     {regex: /([()+-,:\[\]*?])/g, class: 'miql-symbol'},
   ];
 
@@ -20,8 +20,12 @@ export class ColorMIQLPipe implements PipeTransform {
   }
 
   public static transform(miql: string): string {
-    return ColorMIQLPipe.colorCodes.reduce((html: string, colorCode: ColorCode) =>
-      html.replace(colorCode.regex, `<span class="miql ${colorCode.class}">$1</span>`), miql);
+    return ColorMIQLPipe.colorCodes.reduce((html: string, colorCode: ColorCode) => {
+      return html.replace(colorCode.regex, `<span class="miql ${colorCode.class}">$1</span>`);
+    }, miql
+      .replace(/&/g, '&amp')
+      .replace(/</g, '&lt')
+      .replace(/\n/mg, '<br>'));
   }
 
   public static isMIQL(str: string): boolean {

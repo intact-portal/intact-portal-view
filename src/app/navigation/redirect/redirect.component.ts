1f47c715a06116743edbe8f2cc0376b75a182e91
EliotRagueneau <orkeliot@gmail.com>
2021-11-05 17:31:29 +0000

    :bug: Fix redirection on Angular 12

--- src/app/navigation/redirect/redirect.component.ts
+++ src/app/navigation/redirect/redirect.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
-import {forkJoin} from 'rxjs/internal/observable/forkJoin';
+import {UntilDestroy} from '@ngneat/until-destroy';
+import {zip} from 'rxjs';
 
 @UntilDestroy()
 @Component({
@@ -17,24 +17,23 @@ export class RedirectComponent implements OnInit {
   href: string;
 
   ngOnInit() {
-    forkJoin({
-      href: this.route.data,
-      params: this.route.params,
-      queryParams: this.route.queryParams
-    }).pipe(untilDestroyed(this))
-      .subscribe(route => {
-        this.href = route.href.data.externalUrl;
+    zip(
+      this.route.data,
+      this.route.params,
+      this.route.queryParams
+    ).subscribe(([href, params, queryParams]) => {
+      this.href = href.externalUrl;
 
-        for (const param of Object.keys(route.params)) {
-          this.href = this.href.replace(':' + param, route.params[param])
-        }
+      for (const param of Object.keys(params)) {
+        this.href = this.href.replace(':' + param, params[param])
+      }
 
-        const paramList = Object.keys(route.queryParams);
-        if (paramList.length > 0) {
-          this.href += `?${paramList.map(param => `${param}=${route.queryParams[param]}`).join('&')}`;
-        }
+      const paramList = Object.keys(queryParams);
+      if (paramList.length > 0) {
+        this.href += `?${paramList.map(param => `${param}=${queryParams[param]}`).join('&')}`;
+      }
 
-        window.location.href = this.href;
-      });
+      window.location.href = this.href;
+    });
   }
 }

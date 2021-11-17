1fcd6d224ba1f453b79f3b03a48e46ceb62fa94c
EliotRagueneau <159753.AZErty>
2021-11-17 09:52:31 +0000

    :sparkles: Use until destroyed on redirect.component.ts

--- src/app/navigation/redirect/redirect.component.ts
+++ src/app/navigation/redirect/redirect.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {UntilDestroy} from '@ngneat/until-destroy';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {zip} from 'rxjs';
 
 @UntilDestroy()
@@ -21,19 +21,20 @@ export class RedirectComponent implements OnInit {
       this.route.data,
       this.route.params,
       this.route.queryParams
-    ).subscribe(([href, params, queryParams]) => {
-      this.href = href.externalUrl;
+    ).pipe(untilDestroyed(this))
+      .subscribe(([href, params, queryParams]) => {
+        this.href = href.externalUrl;
 
-      for (const param of Object.keys(params)) {
-        this.href = this.href.replace(':' + param, params[param])
-      }
+        for (const param of Object.keys(params)) {
+          this.href = this.href.replace(':' + param, params[param])
+        }
 
-      const paramList = Object.keys(queryParams);
-      if (paramList.length > 0) {
-        this.href += `?${paramList.map(param => `${param}=${queryParams[param]}`).join('&')}`;
-      }
+        const paramList = Object.keys(queryParams);
+        if (paramList.length > 0) {
+          this.href += `?${paramList.map(param => `${param}=${queryParams[param]}`).join('&')}`;
+        }
 
-      window.location.href = this.href;
-    });
+        window.location.href = this.href;
+      });
   }
 }

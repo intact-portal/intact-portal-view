1204fbe3f1eda81693c1b293fdd9c34c066cf80b
EliotRagueneau <eragueneau@ebi.ac.uk>
2022-07-20 17:16:45 +0100

    :sparkles: Add release version and date

--- src/app/home-dashboard/news/news.component.ts
+++ src/app/home-dashboard/news/news.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import pkg from '../../../../package.json';
 import {environment} from '../../../environments/environment';
 import * as d3 from 'd3';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'ip-news',
@@ -12,15 +13,22 @@ export class NewsComponent implements OnInit {
   version = pkg.version;
   releaseDate = 'December 2021';
 
+  release_url = environment.statistics_url + 'release.txt'
+
   stat_url = environment.statistics_url + 'summary_table.csv';
 
+  release: string;
   summary: { [key: string]: number }
 
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+    this.http
+      .get(this.release_url, {responseType: 'text'})
+      .subscribe(value => this.release = value);
+
     d3.csv(this.stat_url, rawRow => ({
       feature: rawRow.Feature,
       amount: +rawRow.Count,

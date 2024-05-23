c53bae7c684ee411aa54b5a7f7660ad3335fe0ff
jmedinaebi <jmedina@ebi.ac.uk>
2024-05-23 12:03:31 +0100

    Survey banner

--- src/app/home-dashboard/home-dashboard.component.ts
+++ src/app/home-dashboard/home-dashboard.component.ts
@@ -10,6 +10,11 @@ export class HomeDashboardComponent implements OnInit {
 
   constructor(private titleService: Title) { }
 
+  surveyBannerMessage = 'Do data resources managed by EMBL-EBI and our collaborators make a difference to your work? \n' +
+      'Please take 10 minutes to fill in our annual user survey, and help us make the case for why sustaining open data resources is critical for life sciences research.\n' +
+      'Survey link: ' +
+      '<a class="vf-banner__link" href="https://www.surveymonkey.com/r/HJKYKTT?channel=[intact]" target="_blank">https://www.surveymonkey.com/r/HJKYKTT</a>';
+
   ngOnInit() {
     this.titleService.setTitle('IntAct');
   }

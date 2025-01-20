bcd8451502b961a41aba16c1162cbe58d28c2774
jmedinaebi <jmedina@ebi.ac.uk>
2025-01-20 15:37:18 +0000

    New feature tab format for single interactions

--- src/app/interactions/shared/model/download/format.model.ts
+++ src/app/interactions/shared/model/download/format.model.ts
@@ -1,15 +1,38 @@
 export class Format {
-  private static _instances: Format[] = [];
 
-  public static tab2_5 = new Format('txt', 'miTab25', 'miTab<sub>2.5</sub>');
-  public static tab2_6 = new Format('txt', 'miTab26', 'miTab<sub>2.6</sub>');
-  public static tab2_7 = new Format('txt', 'miTab27', 'miTab<sub>2.7</sub>');
+  public static tab2_5 = new Format('tsv', 'miTab25', 'miTab<sub>2.5</sub>');
+  public static tab2_6 = new Format('tsv', 'miTab26', 'miTab<sub>2.6</sub>');
+  public static tab2_7 = new Format('tsv', 'miTab27', 'miTab<sub>2.7</sub>');
   public static xml2_5 = new Format('xml', 'miXML25', 'miXML<sub>2.5</sub>');
   public static xml3_0 = new Format('xml', 'miXML30', 'miXML<sub>3.0</sub>');
   public static json = new Format('json', 'miJSON', 'miJSON<sub>1.0</sub>');
+  public static featureTab = new Format('tsv', 'featureTab', 'featureTab<sub>1.0</sub>');
 
-  static get instances(): Format[] {
-    return [...this._instances];
+  private static _interactionListFormats: Format[] = [
+    Format.tab2_5,
+    Format.tab2_6,
+    Format.tab2_7,
+    Format.xml2_5,
+    Format.xml3_0,
+    Format.json,
+  ];
+
+  private static _singleInteractionFormats: Format[] = [
+    Format.tab2_5,
+    Format.tab2_6,
+    Format.tab2_7,
+    Format.xml2_5,
+    Format.xml3_0,
+    Format.json,
+    Format.featureTab,
+  ];
+
+  static get interactionListFormats(): Format[] {
+    return [...this._interactionListFormats];
+  }
+
+  static get singleInteractionFormats(): Format[] {
+    return [...this._singleInteractionFormats];
   }
 
   public readonly extension: string;
@@ -20,6 +43,5 @@ export class Format {
     this.extension = extension;
     this.name = name;
     this.html = html;
-    Format._instances.push(this);
   }
 }
